1e535a1a0bf4ca3c6334823fd3c75c81bbf676e8
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-03 11:01:03 +0200

    Added checking on which button user clicked.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log("main.js loaded");
+"use strict"
+let form = document.querySelector("form");
+
+function checkClick(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	let button = e.target.className;
+	console.log(button);
+}
+
+form.addEventListener("click", checkClick, false);
+
