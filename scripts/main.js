29274d6facd57455d38853073c148302eb3a6fa1
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-03 11:26:15 +0200

    Added clearInterval when user stopBtutton and some another code

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,36 @@
 "use strict"
-let form = document.querySelector("form");
+let form = document.querySelector("form"),
+    stopwatchEl = document.querySelector("stopwatch"),
+	time = 0,
+	intervalId;
 
 function checkClick(e) {
 	e.preventDefault();
 	e.stopPropagation();
 	let button = e.target.className;
 	console.log(button);
+	if (button === "startBtn") {
+		intervalId = startStopwatch();
+		console.log(intervalId);
+	} else if (button === "stopBtn"){
+		clearInterval(intervalId);
+	} else if (button === "resetButton"){
+		
+	} else if (button === "clearLogButton"){
+		
+	}
+};
+
+function startStopwatch(){
+	let intervalId = setInterval (timeGo, 100);
+	return intervalId;
+};
+
+function timeGo(){
+	console.log("timeGo");
 }
+
+
 
 form.addEventListener("click", checkClick, false);
 
